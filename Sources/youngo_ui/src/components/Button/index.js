07263db5fd571e238f3ae2c8ec33e2d67253de8d
ahntuann <ahntuan284@gmail.com>
2024-08-16 15:03:24 +0700

    Cài đặt customize cra để cấu hình bên trong, cài đặt babel plug in

---
+++ Sources/youngo_ui/src/components/Button/index.js
@@ -0,0 +1,9 @@
+import myAlert from '~/myalert'
+
+function Button() {
+    return (
+        <button onClick={myAlert}>Click me</button>
+    )
+}
+
+export default Button
---
+++ Sources/youngo_ui/src/myalert.js
@@ -0,0 +1,5 @@
+const myAlert = () => {
+    alert('hello')
+}
+
+export default myAlert
