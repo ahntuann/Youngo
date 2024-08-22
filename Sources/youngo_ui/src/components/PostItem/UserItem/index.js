d1e36567ef58b8609e05e765fb88369fea2446d2
ahntuann <ahntuan284@gmail.com>
2024-08-22 13:59:52 +0700

    Build logic: fetch post and user of the post

---
+++ Sources/youngo_ui/src/components/PostItem/UserItem/index.js
@@ -0,0 +1,16 @@
+import classNames from 'classnames/bind';
+
+import style from '../PostItem.module.scss';
+
+const cs = classNames.bind(style);
+
+function UserItem({ user }) {
+    return (
+        <div className={cs('wrapper')}>
+            {user && <img className={cs('user-avt')} src={user.image} />}
+            {user && <h4 className={cs('user-name')}>{user.username}</h4>}
+        </div>
+    );
+}
+
+export default UserItem;
--- Sources/youngo_ui/src/pages/Home/index.js
+++ Sources/youngo_ui/src/pages/Home/index.js
@@ -1,11 +1,37 @@
 import classNames from 'classnames/bind';
 
 import style from './Home.module.scss';
+import { useEffect, useState } from 'react';
+import PostItem from '~/components/PostItem';
 
 const cs = classNames.bind(style);
 
 function Home() {
-    return <div className={cs('wrapper')}></div>;
+    const [curPosts, setCurPosts] = useState(() => []);
+
+    useEffect(() => {
+        async function fetchPost() {
+            try {
+                const res = await fetch('https://dummyjson.com/posts/?limit=10');
+                const posts = await res.json();
+
+                return posts.posts;
+            } catch (error) {
+                console.log('Something wrong...');
+            }
+        }
+
+        fetchPost().then((posts) => {
+            console.log(posts);
+            setCurPosts(posts);
+        });
+    }, []);
+
+    return (
+        <div className={cs('wrapper')}>
+            <PostItem posts={curPosts} />
+        </div>
+    );
 }
 
 const currentUser = '/@babibong_28';
